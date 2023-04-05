28403bb0729783fa39c4d53b682f1638c8e9fce0
DavidNart90 <davidnart90@gmail.com>
2023-04-05 23:41:12 +0000

    Script Change

--- Script/script.js
+++ Script/script.js
@@ -41,7 +41,7 @@ ScrollReveal({
     reset: false,
     distance: '80px',
     duration: 2000,
-    delay: 200,
+    delay: 250,
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
